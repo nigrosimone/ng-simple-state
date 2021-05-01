66a0f5015cde3bc26171f5270ee087aa9eba766a
Nigro Simone <nigro.simone@gmail.com>
2021-05-01 13:54:50 +0200

    fix test

--- projects/ng-simple-state/src/lib/ng-simple-state-base-store.spec.ts
+++ projects/ng-simple-state/src/lib/ng-simple-state-base-store.spec.ts
@@ -3,21 +3,6 @@ import { Observable } from 'rxjs';
 import { NgSimpleStateBaseStore } from './ng-simple-state-base-store';
 import { NgSimpleStateDevTool } from './ng-simple-state-dev-tool';
 
-class DevToolsExtension {
-    name = null;
-    state = null;
-    
-    connect() {
-        const self = this;
-        return {
-            send: (name: string, state: any) => {
-                self.name = name;
-                self.state = state;
-            }
-        }
-    }
-};
-
 export interface CounterState {
     count: number;
 }
@@ -50,8 +35,7 @@ describe('NgSimpleStateBaseStore', () => {
     let service: CounterStore;
 
     beforeEach(() => {
-        window["devToolsExtension"] = new DevToolsExtension();
-        service = new CounterStore(new NgSimpleStateDevTool({enableDevTool: true}));
+        service = new CounterStore(new NgSimpleStateDevTool());
     });
 
 
@@ -68,8 +52,6 @@ describe('NgSimpleStateBaseStore', () => {
         service.selectState(state => state.count).subscribe(value => {
             expect(value).toBe(2);
             expect(service.getCurrentState()).toEqual({count: 2});
-            expect(window["devToolsExtension"].name).toBe('CounterStore.increment');
-            expect(window["devToolsExtension"].state).toEqual({count: 2});
             done();
         });
     });
@@ -79,8 +61,6 @@ describe('NgSimpleStateBaseStore', () => {
         service.selectState(state => state.count).subscribe(value => {
             expect(value).toBe(0);
             expect(service.getCurrentState()).toEqual({count: 0});
-            expect(window["devToolsExtension"].name).toBe('CounterStore.decrement');
-            expect(window["devToolsExtension"].state).toEqual({count: 0});
             done();
         });
     });
@@ -98,8 +78,6 @@ describe('NgSimpleStateBaseStore', () => {
         service.selectState(state => state.count).subscribe(value => {
             expect(value).toBe(5);
             expect(service.getCurrentState()).toEqual({count: 5});
-            expect(window["devToolsExtension"].name).toBe('test');
-            expect(window["devToolsExtension"].state).toEqual({count: 5});
             done();
         });
     });
@@ -107,61 +85,6 @@ describe('NgSimpleStateBaseStore', () => {
     it('completeState', () => {
         service.completeState();
         expect(service.getCurrentState()).toEqual({count: 1});
-    });
-
-    it('no dev tool', (done) => {
-        const _service = new CounterStore(new NgSimpleStateDevTool({enableDevTool: false}));
-        _service.setState(() => ({ count: 5 }), 'test');
-        _service.selectState(state => state.count).subscribe(value => {
-            expect(value).toBe(5);
-            expect(_service.getCurrentState()).toEqual({count: 5});
-            expect(window["devToolsExtension"].name).toEqual(null);
-            expect(window["devToolsExtension"].state).toEqual(null);
-            done();
-        });
-    });
-
-    it('no dev tool 2', (done) => {
-        const _service = new CounterStore(new NgSimpleStateDevTool());
-        _service.setState(() => ({ count: 5 }), 'test');
-        _service.selectState(state => state.count).subscribe(value => {
-            expect(value).toBe(5);
-            expect(_service.getCurrentState()).toEqual({count: 5});
-            expect(window["devToolsExtension"].name).toEqual(null);
-            expect(window["devToolsExtension"].state).toEqual(null);
-            done();
-        });
-    });
-
-    it('no dev tool 3', (done) => {
-        window["devToolsExtension"] = null;
-        const _service = new CounterStore(new NgSimpleStateDevTool());
-        _service.setState(() => ({ count: 5 }), 'test');
-        _service.selectState(state => state.count).subscribe(value => {
-            expect(value).toBe(5);
-            expect(_service.getCurrentState()).toEqual({count: 5});
-            expect(window["devToolsExtension"]).toEqual(null);
-            done();
-        });
-    });
-
-    it('no dev tool 4', (done) => {
-        window["devToolsExtension"] = { 
-            name: null,
-            state: null,
-            connect: () => {
-                return null;
-            }
-        };
-        const _service = new CounterStore(new NgSimpleStateDevTool({enableDevTool: true}));
-        _service.setState(() => ({ count: 5 }), 'test');
-        _service.selectState(state => state.count).subscribe(value => {
-            expect(value).toBe(5);
-            expect(_service.getCurrentState()).toEqual({count: 5});
-            expect(window["devToolsExtension"].name).toEqual(null);
-            expect(window["devToolsExtension"].state).toEqual(null);
-            done();
-        });
     });
 
 });
