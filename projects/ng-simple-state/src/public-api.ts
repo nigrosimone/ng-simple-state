2b21488fb207623ba9f6adbeb1d3358c63c7e512
Nigro Simone <nigro.simone@gmail.com>
2021-05-02 21:45:39 +0200

    eport local storage service

--- projects/ng-simple-state/src/public-api.ts
+++ projects/ng-simple-state/src/public-api.ts
@@ -4,4 +4,5 @@
 export * from './lib/ng-simple-state.module';
 export * from './lib/ng-simple-state-base-store';
 export * from './lib/ng-simple-state-dev-tool';
+export * from './lib/ng-simple-state-local-storage';
 export * from './lib/ng-simple-state-models';
