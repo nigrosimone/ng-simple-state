c55288296fe0ce39114d826b5bb509c0253e6790
Nigro Simone <nigro.simone@gmail.com>
2023-04-10 09:17:18 +0200

    docs

--- projects/ng-simple-state/src/lib/ng-simple-state-models.ts
+++ projects/ng-simple-state/src/lib/ng-simple-state-models.ts
@@ -4,9 +4,21 @@ import { InjectionToken } from '@angular/core';
  * NgSimpleState config option
  */
 export interface NgSimpleStateConfig {
+    /**
+     * if `true` enable `Redux DevTools` browser extension for inspect the state of the store.
+     */
     enableDevTool?: boolean;
+    /**
+     * if `true` latest state of store is saved in local storage and reloaded on store initialization.
+     */
     enableLocalStorage?: boolean;
+    /**
+     * Set the persistent storage `local` or `session`.
+     */
     persistentStorage?: 'session' | 'local';
+    /**
+     * A function used to compare the previous and current state for equality. 
+     */
     comparator?: <K>(previous: K, current: K) => boolean;
 }
 
