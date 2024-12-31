3c73a03ce2b6feb34db9177e1246f23cb969fdf0
Nigro Simone <nigro.simone@gmail.com>
2024-12-31 03:50:33 +0100

    fix: improve type annotations and enhance stateComparator function for better readability

--- projects/ng-simple-state/src/lib/ng-simple-state-utils.ts
+++ projects/ng-simple-state/src/lib/ng-simple-state-utils.ts
@@ -31,10 +31,11 @@ export function stateComparator<K>(a: K, b: K): boolean {
     if (a && b && typeof a == 'object' && typeof b == 'object') {
         if (a.constructor !== b.constructor) return false;
 
-        let length, i, keys;
+        let length: number;
+        let i: number;
         if (Array.isArray(a) && Array.isArray(b)) {
             length = a.length;
-            if (length != b.length) return false;
+            if (length !== b.length) return false;
             for (i = length; i-- !== 0;)
                 if (!stateComparator(a[i], b[i])) return false;
             return true;
@@ -44,7 +45,7 @@ export function stateComparator<K>(a: K, b: K): boolean {
         if (a.valueOf !== Object.prototype.valueOf) return a.valueOf() === b.valueOf();
         if (a.toString !== Object.prototype.toString) return a.toString() === b.toString();
 
-        keys = Object.keys(a);
+        const keys = Object.keys(a);
         length = keys.length;
         if (length !== Object.keys(b).length) return false;
 
