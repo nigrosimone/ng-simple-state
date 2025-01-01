341d43c289ceec8ee23c044e1a1a94a28a317c0c
Nigro Simone <nigro.simone@gmail.com>
2025-01-01 10:01:50 +0100

    fix: move HeroService provider from AppComponent to TourComponent

--- projects/ng-simple-state-demo/src/app/app.component.ts
+++ projects/ng-simple-state-demo/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { HeroService } from './tour-of-heroes/hero.service';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -23,7 +22,6 @@ import { HeroService } from './tour-of-heroes/hero.service';
     CommonModule,
     RouterModule
   ],
-  providers: [HeroService],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
--- projects/ng-simple-state-demo/src/app/tour-of-heroes/tour.component.ts
+++ projects/ng-simple-state-demo/src/app/tour-of-heroes/tour.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { HeroService } from './hero.service';
 
 @Component({
   /* eslint-disable-next-line @angular-eslint/component-selector */
@@ -9,6 +9,7 @@ import { RouterModule } from '@angular/router';
   templateUrl: './tour.component.html',
   styleUrls: ['./tour.component.css'],
   imports: [RouterModule, CommonModule],
+  providers: [HeroService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TourComponent {
