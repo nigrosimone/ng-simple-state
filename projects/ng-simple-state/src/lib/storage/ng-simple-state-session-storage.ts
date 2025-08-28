ac9c2ac36feca26ba5ce3fc18062823e1bad91a8
Nigro Simone <nigro.simone@gmail.com>
2025-08-28 08:07:34 +0200

    fix: circular deps

--- projects/ng-simple-state/src/lib/storage/ng-simple-state-session-storage.ts
+++ projects/ng-simple-state/src/lib/storage/ng-simple-state-session-storage.ts
@@ -1,6 +1,7 @@
 
+import { NgSimpleStateStoreConfig } from '../ng-simple-state-models';
 import { NgSimpleStateBrowserStorage } from './ng-simple-state-browser-storage';
-import { NgSimpleStateStoreConfig } from 'ng-simple-state';
+
 export class NgSimpleStateSessionStorage<K = any> extends NgSimpleStateBrowserStorage<K> {
     constructor(config?: NgSimpleStateStoreConfig<K>) {
         super(sessionStorage, config);
