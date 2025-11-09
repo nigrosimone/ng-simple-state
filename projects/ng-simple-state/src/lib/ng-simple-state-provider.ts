233b9f8d0fb888c96e9a5b2475ded658b4d5d3a2
Nigro Simone <nigro.simone@gmail.com>
2025-11-09 21:01:30 +0100

    docs: improve

--- projects/ng-simple-state/src/lib/ng-simple-state-provider.ts
+++ projects/ng-simple-state/src/lib/ng-simple-state-provider.ts
@@ -1,13 +1,19 @@
 import { EnvironmentProviders, makeEnvironmentProviders } from "@angular/core";
 import { NG_SIMPLE_STORE_CONFIG, NgSimpleStateConfig } from "./ng-simple-state-models";
 
+/**
+ * Provide NgSimpleState with optional global configuration
+ * @param {NgSimpleStateConfig} ngSimpleStateConfig 
+ * @returns {EnvironmentProviders[]}
+ */
 export function provideNgSimpleState(ngSimpleStateConfig?: NgSimpleStateConfig): EnvironmentProviders[] {
-    const providers: EnvironmentProviders[] = [];
     if (ngSimpleStateConfig) {
-        providers.push(makeEnvironmentProviders([{
-            provide: NG_SIMPLE_STORE_CONFIG,
-            useValue: ngSimpleStateConfig,
-        }]));
+        return [
+            makeEnvironmentProviders([{
+                provide: NG_SIMPLE_STORE_CONFIG,
+                useValue: ngSimpleStateConfig,
+            }])
+        ];
     }
-    return providers;
+    return [];
 }
