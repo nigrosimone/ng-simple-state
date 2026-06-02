fdd0ba1faa158b962945ddb975e90f485a88b64b
Nigro Simone <nigro.simone@gmail.com>
2026-06-02 19:34:51 +0200

    chore: update demos

--- projects/ng-simple-state-demo/src/app/app.component.ts
+++ projects/ng-simple-state-demo/src/app/app.component.ts
@@ -1,21 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { RouterModule } from '@angular/router';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-root',
   template: `<h1>NgSimpleState demos</h1>
-  <nav>
-      <ul>
-        <li><a routerLink="/todo" routerLinkActive="active">Todo</a></li>
-        <li><a routerLink="/counter" routerLinkActive="active">Counter</a></li>
-        <li><a routerLink="/tour" routerLinkActive="active">Tour of heroes</a></li>
-        <li><a routerLink="/features" routerLinkActive="active">New Features (v21.1.0+)</a></li>
-      </ul>
-  </nav>
-  <hr />
   <router-outlet></router-outlet>
   `,
   imports: [
--- projects/ng-simple-state-demo/src/main.ts
+++ projects/ng-simple-state-demo/src/main.ts
@@ -6,9 +6,11 @@ import { provideRouter, Routes } from '@angular/router';
 import isEqual from 'lodash.isequal';
 
 const routes: Routes = [
-  { path: 'todo', loadComponent: () => import('./app/todo/todo.component').then(m => m.TodoComponent) },
-  { path: 'counter', loadComponent: () => import('./app/counter/counter.component').then(m => m.CounterComponent) },
-  { path: 'tour', loadChildren: () => import('./app/tour-of-heroes/tour-routing').then(m => m.routes) },
+  {
+    path: '',
+    redirectTo: 'features',
+    pathMatch: 'full'
+  },
   {
     path: 'features',
     loadComponent: () => import('./app/features/features.component').then(m => m.FeaturesComponent),
