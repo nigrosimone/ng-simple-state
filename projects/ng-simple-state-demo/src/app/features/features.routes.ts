8c85bda05c6354f6b874042a55c02cfe0b2138a6
Nigro Simone <nigro.simone@gmail.com>
2026-06-07 16:08:46 +0200

    chore: clean up

--- projects/ng-simple-state-demo/src/app/features/features.routes.ts
+++ projects/ng-simple-state-demo/src/app/features/features.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const featuresRoutes: Routes = [
-  { path: '', redirectTo: 'effects', pathMatch: 'full' },
   { path: 'effects', loadComponent: () => import('./effects/effects-demo.component').then(m => m.EffectsDemoComponent) },
   { path: 'linked-signals', loadComponent: () => import('./linked-signals/linked-signals-demo.component').then(m => m.LinkedSignalsDemoComponent) },
   { path: 'plugins', loadComponent: () => import('./plugins/plugins-demo.component').then(m => m.PluginsDemoComponent) },
